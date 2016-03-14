f51b67ae252c53f131c678457319f08f3447b4fa
Joey <joe.laird1@gmail.com>
2016-03-14 15:39:48 -0400

    fixed Rock.png

--- js/engine.js
+++ js/engine.js
@@ -173,7 +173,7 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/rock.png',
+        'images/Rock.png',
         'images/char-boy.png'
 
     ]);
